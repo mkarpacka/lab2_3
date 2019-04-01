ba09b17d5f9b136d6e68d12314ef6301c0d167f5
mkarpacka <209348@edu.p.lodz.pl>
2019-04-01 20:53:50 +0200

    added SequenceSearcherDoubler and SimilarityFinderTest

--- src/test/java/SequenceSearcherDoubler.java
+++ src/test/java/SequenceSearcherDoubler.java
@@ -1,5 +1,24 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SequenceSearcherDoubler {
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SearchResult.Builder;
+import edu.iis.mto.search.SequenceSearcher;
 
+public class SequenceSearcherDoubler implements SequenceSearcher{
+
+    public static int searchMethodCounter = 0;
+
+    @Override public SearchResult search(int key, int[] seq) {
+        Builder builder = SearchResult.builder();
+        searchMethodCounter++;
+        int position = 0;
+        for(int i = 0; i < seq.length; i++) {
+            if(key == seq[i]) {
+                builder.withFound(true);
+                builder.withPosition(i);
+            }
+        }
+        return builder.build();
+    }
 }
--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -1,5 +1,30 @@
-package PACKAGE_NAME;
+import edu.iis.mto.similarity.SimilarityFinder;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class SimilarityFinderTest {
+
+    SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherDoubler());
+    SequenceSearcherDoubler sequenceSearcherDoubler = new SequenceSearcherDoubler();
+
+    @Test
+    public void isReturnedValueEqualOneWithTheSameSequences(){
+        int[] seq1 = {1, 2, 3};
+        int[] seq2 = {1, 2, 3};
+
+        double jackardSimilarity = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        Assert.assertThat(jackardSimilarity, Matchers.equalTo(1.0));
+
+    }
+
+    @Test
+    public void isReturnedOneWithEmptySequences(){
+        int[] seq1 = {};
+        int[] seq2 = {};
+
+        double jackardSimilarity = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        Assert.assertThat(jackardSimilarity, Matchers.equalTo(1.0));
+    }
 
 }
